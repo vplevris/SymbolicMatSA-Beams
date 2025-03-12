1825d0f8b9413a72c12a696e23d0adcc27cd5b97
vplevris <81329216+vplevris@users.noreply.github.com>
2025-03-13 00:08:26 +0300

    Update and rename main_symbolic_MSA_beams.m to main_symbolic_MatSA_beams.m

--- main_symbolic_MatSA_beams.m
+++ main_symbolic_MatSA_beams.m
@@ -1,6 +1,6 @@
 % Deriving Analytical Solutions Using Symbolic Matrix Structural Analysis:
 % Part 1 – Continuous Beams
-% Version 1.0 - 6 November 2024
+% Version 1.2 - 13 March 2025
 % By Vagelis Plevris - vplevris@gmail.com
 
 clear all, close all, clc
